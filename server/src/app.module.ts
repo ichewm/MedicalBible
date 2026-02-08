1abe2fbe34159e8f2b7da008337388af70fcc3a4
Ralph Wiggum <ralph@wiggum.cc>
2026-02-09 02:46:33 +0800

    execution - engineering.software-engineer

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -33,6 +33,7 @@ import { NotificationModule } from "./modules/notification/notification.module";
 import { PaymentModule } from "./modules/payment/payment.module";
 import { StorageModule } from "./modules/storage/storage.module";
 import { ChatModule } from "./modules/chat/chat.module";
+import { DataExportModule } from "./modules/data-export/data-export.module";
 
 // 公共模块导入
 import { RedisModule } from "./common/redis/redis.module";
@@ -135,6 +136,7 @@ class HealthController {
     NotificationModule, // 通知模块（邮件/短信）
     PaymentModule, // 支付模块
     ChatModule, // 客服模块
+    DataExportModule, // 数据导出模块
   ],
   providers: [
     // 全局 JWT 认证守卫
